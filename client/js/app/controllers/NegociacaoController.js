8e79e8abeeb69b7e2115150d7311129273c296d7
João Victor Anastácio <profissional.anastacio@gmail.com>
2019-07-09 16:39:39 -0300

    feat(ListaNegociacoes, NegociacaoController): add data structure

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,17 +6,28 @@ class NegociacaoController {
         this._inputData = $('#data')
         this._inputQuantidade = $('#quantidade')
         this._inputValor = $('#valor')
+        this._listaNegociacoes = new ListaNegociacoes()
     }
 
     adiciona(event) {
         event.preventDefault()
 
-        let negociacao = new Negociacao(
+        this._listaNegociacoes.adiciona(this._criaNegociacao())
+        this._limpaformulario()
+    }
+
+    _criaNegociacao() {
+        return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
         )
+    }
 
-        console.log(negociacao)
+    _limpaformulario() {
+        this._inputData.value = ""
+        this._inputQuantidade.value = 1
+        this._inputValor.value = 0.0
+        this._inputData.focus()
     }
 }
